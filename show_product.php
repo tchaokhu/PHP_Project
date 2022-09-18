dd7da82f4905482d9bdab7aeb7140cca41031b1d
Thanapon Chaokhuwiang <nrnutthanapon@gmail.com>
2022-09-18 14:02:52 +0700

    push to heroku

--- show_product.php
+++ show_product.php
@@ -23,8 +23,8 @@ if(mysqli_num_rows($result)>0){
     echo "<table border=1><tr><th>id</th><th>name</th><th>description</th><th>price</th><th></th></tr>";
     while($row=mysqli_fetch_assoc($result)){
     echo "<tr><td>".$row["id"]."</td><td>".$row["name"]."</td><td>";
-    echo $row["des"]."</td><td>".$row["price"]."</td>";
-    echo "<td><a href='add_product.php?id=".$row["id"]."'>ใส่ตระกร้า</a></td></tr>";
+    echo $row["des"]."</td><td>".$row["price"]."</td></tr>";
+    //echo "<td><a href='add_product.php?id=".$row["id"]."'>ใส่ตระกร้า</a></td></tr>";
     }
     echo "</table>";
     $next = ($start_page/$per_page)+1;
@@ -39,24 +39,24 @@ if(mysqli_num_rows($result)>0){
 }else{
     echo "0 results";
 }
-if(isset($_SESSION["cart"])){
-$total=0;
-echo "<h1>ตระกร้าสินค้า</h1>";
-echo "<table><tr><th>ลำดับ</th><th>id</th><th>name</th><th>description</th><th>price</th></tr>";
-    for($i=0;$i<count($_SESSION["cart"]);$i++)
-    {
-        $item=$_SESSION["cart"][$i];
-        echo "<tr><td>".($i+1)."</td>";
-        echo "<td>".$item['id']."</td>";
-        echo "<td>".$item['name']."</td>";
-        echo "<td>".$item['des']."</td>";
-        echo "<td>".$item['price']."</td></tr>";
-        echo "<td><a href='del_cart.php?i=".$i."'>";
-        echo "<font color='red'>x</font></a></td></tr>";
-        $total+=$item['price'];
-    }
-echo "</table>";
-echo "<h1>ราคาสิ้นค้า $total บาท</h1>";
-echo "<h2><a href = 'checkout.php'>สั่งซื้อ</a></h2>";
-}
+// if(isset($_SESSION["cart"])){
+// $total=0;
+// echo "<h1>ตระกร้าสินค้า</h1>";
+// echo "<table><tr><th>ลำดับ</th><th>id</th><th>name</th><th>description</th><th>price</th></tr>";
+//     for($i=0;$i<count($_SESSION["cart"]);$i++)
+//     {
+//         $item=$_SESSION["cart"][$i];
+//         echo "<tr><td>".($i+1)."</td>";
+//         echo "<td>".$item['id']."</td>";
+//         echo "<td>".$item['name']."</td>";
+//         echo "<td>".$item['des']."</td>";
+//         echo "<td>".$item['price']."</td></tr>";
+//         echo "<td><a href='del_cart.php?i=".$i."'>";
+//         echo "<font color='red'>x</font></a></td></tr>";
+//         $total+=$item['price'];
+//     }
+// echo "</table>";
+// echo "<h1>ราคาสิ้นค้า $total บาท</h1>";
+// echo "<h2><a href = 'checkout.php'>สั่งซื้อ</a></h2>";
+// }
 mysqli_close($con);
